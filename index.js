7238789f67f75e5c4480674d5ea6028e7c46fc49
Jack Tang <jackhftang@hotmail.com>
2015-04-20 00:42:20 +0800

    add history

--- index.js
+++ index.js
@@ -23,11 +23,16 @@ server.route({
 });
 
 // socket handler
+var history = [];
 io.on('connection', function(socket){
   log('new connection');
 
+  socket.emit('msg', history);
+
   socket.on('msg', function(msg){
     log(msg);
+    history.push(msg);
+    while(history.length > 5) history.shift();
     io.emit('msg', msg);
   });
 
